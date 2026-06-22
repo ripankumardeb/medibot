c74320c1e25743a3ea0fd29e8ab354b168cd45d3
nn8ninsyssky <nn8ninsyssky@gmail.com>
2026-06-22 19:10:21 +0530

    Webapp build successfull

--- app.py
+++ app.py
@@ -173,9 +173,10 @@ def chat():
 # Main Entry
 # ---------------------------------------------------------
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         port=8080,
-        debug=True
+        debug=True,
+        use_reloader=False
     )
--- src/helper.py
+++ src/helper.py
@@ -1,43 +1,52 @@
-from langchain.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
 from typing import List
-from langchain.schema import Document
-from langchain.embeddings import HuggingFaceEmbeddings
 
 
 def load_pdf_files(data):
-    loader=DirectoryLoader(
+    loader = DirectoryLoader(
         data,
         glob="*.pdf",
         loader_cls=PyPDFLoader
     )
+
     documents = loader.load()
     return documents
 
-def filter_to_minimal_docs(docs:List[Document])->List[Document]:
-    minimal_docs:List[Document]=[]
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs: List[Document] = []
+
     for doc in docs:
-        src =doc.metadata.get("source")
+        src = doc.metadata.get("source")
+
         minimal_docs.append(
             Document(
                 page_content=doc.page_content,
-                metadata={"source":src}
+                metadata={"source": src}
             )
         )
+
     return minimal_docs
 
+
 def text_split(minimal_docs):
-    text_splitter=RecursiveCharacterTextSplitter(
+    text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
-        chunk_overlap=20,
-        
+        chunk_overlap=20
     )
-    texts_chunk=text_splitter.split_documents(minimal_docs)
+
+    texts_chunk = text_splitter.split_documents(minimal_docs)
     return texts_chunk
+
 
 def download_embeddings():
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    embeddings=HuggingFaceEmbeddings(
-        model_name=model_name,
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name
     )
+
     return embeddings
